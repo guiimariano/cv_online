ea86a8650ea4e2521b37a1423af3c368c264850d
guiimariano <pro.gmariano@gmail.com>
2023-03-01 12:16:27 -0300

    feat: create component about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AboutmeComponent } from './shared/aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
